aec9728c2f2e05d39c1d5b68ae9cedde6b44b811
IBF Comunicação <comunicacaoibf@hotmail.com>
2017-11-16 14:54:39 -0300

    exclusões de ités que eu não usarei no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -16,8 +15,6 @@ import { IntroPageModule } from '../pages/intro/intro.module';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
@@ -30,8 +27,6 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
     
